b7a945fc13b23a37631250013974fe44d19004ba
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-03 19:21:29 +0200

    Create index.js

---
+++ Frontend/src/index.js
@@ -0,0 +1,12 @@
+// frontend/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Ponto de entrada da aplicação React
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
